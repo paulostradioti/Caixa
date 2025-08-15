97af6b0ae58a1c15b37c5cb9b29833c2ee3a7a4d
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-08-14 22:11:16 -0300

    Finish upload

--- Caixa.Web/Controllers/UploadController.cs
+++ Caixa.Web/Controllers/UploadController.cs
@@ -13,7 +13,7 @@ namespace Caixa.Web.Controllers
         [HttpPost]
         public IActionResult Upload(UploadViewModel uploadModel)
         {
-            var extensions = new[] {  ".txt", ".pdf"};
+            var extensions = new[] { ".exe", ".txt", ".pdf", ".mp4", ".png" };
             var fileExtention = Path.GetExtension(uploadModel.Arquivo.FileName);
             if (!extensions.Contains(fileExtention))
             {
@@ -21,7 +21,17 @@ namespace Caixa.Web.Controllers
                 return View("Index", uploadModel);
             }
 
-            return Ok();
+            var name = Path.GetFileNameWithoutExtension(uploadModel.Arquivo.FileName);
+            var safeName = $"{name}-{Guid.NewGuid():N}{fileExtention}";
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "uploads", safeName);
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+            using var stream = System.IO.File.Create(fullPath);
+            uploadModel.Arquivo.CopyTo(stream);
+
+
+            TempData["mensagem"] = "Sucesso!";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
